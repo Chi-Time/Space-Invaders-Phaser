7290faf224aa65533f8c26df4a2eb1d81cc8d71f
Achromic <music1384@yahoo.co.uk>
2016-03-27 22:08:55 +0100

    Added some initial global dependencies for prefab and game logic creation.

---
+++ Space Invaders/js/globaldependencies.js
@@ -0,0 +1,26 @@
+// The player object.
+var player;
+// The player's speed.
+var playerSpeed = 65;
+// The enemy object.
+var enemy;
+// The enemy row group.
+var rowGroup;
+// The entire enemy wave group.
+var waveGroup;
+// The player's individual bullet object.
+var playerBullet;
+// The player's bullet group.
+var playerBulletGroup;
+// The enemy's individiual bullet object.
+var enemyBullet;
+// The enemy's bullet group.
+var enemyBulletGroup;
+// Keyboard controls.
+var cursors;
+// The game's score.
+var score;
+var fpsText;
+// The player's number of lives.
+var LIVES;
+
