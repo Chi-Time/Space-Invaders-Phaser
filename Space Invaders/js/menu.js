fe89bebc04c07130e4d2c5c819fde277ebedf503
Achromic <music1384@yahoo.co.uk>
2016-03-27 16:16:42 +0100

    Added simple text label to understand when the menu state is loaded.

--- Space Invaders/js/menu.js
+++ Space Invaders/js/menu.js
@@ -1,18 +1,22 @@
+// Define the menu state.
 MenuState = function (game)
 {
     
 };
 
+// Extend the menu state.
 MenuState.prototype =
 {
     create : function ()
     {
+        var menuLabel = game.add.text(80, 150, 'Click to start', {font: '20px Courier', fill: '#ffffff'});
         // Create a listener event for a mouse click or touch and executes the function when the event occurs.
         game.input.onDown.add(this.start);
     },
     
     start : function ()
     {
+        // Start the main game state.
         game.state.start ("Game");
     },
 }
