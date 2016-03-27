10c7a22a02b4150a2a159be7da4b150719dc58f5
Achromic <music1384@yahoo.co.uk>
2016-03-27 16:16:21 +0100

    Added loading label to understand when game is within the boot state.

--- Space Invaders/js/boot.js
+++ Space Invaders/js/boot.js
@@ -5,17 +5,22 @@ BootState = function (game)
 };
 
 
-// Extend our boot state to contain extra function.
+// Extend our boot state.
 BootState.prototype =
 {
+    // Preloads all required assets before the game starts.
     preload : function ()
     {
+        // Create a loadling label to display to the user informing that loading is occuring.
+		var loadingLabel = game.add.text(80, 150, 'loading...', {font: '30px Courier', fill: '#ffffff'});
         game.load.image("Enemy", "assets/sprites/Food_Spr.png");
         game.load.image("Player", "assets/sprites/Snake Head_Spr.png");
     },
     
+    // Called directly after preload has finished loading.
     create : function ()
     {
+        // Display the menu.
         game.state.start ("Menu");
     }
 }
