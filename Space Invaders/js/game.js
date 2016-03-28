b2e24f99ef869721c39e1bfc3b23d366c68fadeb
Achromic <music1384@yahoo.co.uk>
2016-03-28 13:51:41 +0100

    Removed old skeleton tests and added the player object and it's input polling. Also added alien object and have begun testing alien's functions.

--- Space Invaders/js/game.js
+++ Space Invaders/js/game.js
@@ -1,77 +1,148 @@
-var barrierGroup;
-var testObject;
-var barrierFallSpeed = 40;
-
+// Define the game state.
 GameState = function (game)
 {
     
 };
 
+// Extend the game state.
 GameState.prototype =
 {
     create : function ()
     {
+        this.initGameDefaults();
+        this.initPhysics();
+        this.initGraphics();
+        this.initObjects();
+        this.initInput();
+        this.initEventHandlers();
+        this.initAudio();
+        this.initUI();
         
-        testObject = new mainObject(game, 4, 0);
-        var notPlayer = new mainObject (game, 8, 0);
+        // Enable checking of advancedTiming for debug.
+        this.game.time.advancedTiming = true;
+    },
+    
+    // Initialise the game's default values.
+    initGameDefaults : function ()
+    {
+        // Set the initial game score.
+        score = 0;
+        // Set the initial lives count.
+        lives = 3;
+    },
+    
+    //DEBUG FUNCTION - test's whether a player can be respawned through the use of Phaser.reset().\\
+    addCharacter : function ()
+    {
+        player.RespawnPlayer(16, 16);
+    },
+    //DEBUG FUNCTION - test's whether a player can be killed through the use of Phaser.kill().\\
+    removeCharacter : function ()
+    {
+        player.KillPlayer();
+    },
+    
+    // Initialise the game's physics system.
+    initPhysics : function ()
+    {
+        // Start the physics system for use in the whole game.
+        game.physics.startSystem(Phaser.Physics.ARCADE);
+    },
+    
+    // Initiliase the graphics for the game.
+    initGraphics : function ()
+    {
         
-        game.add.existing(testObject);
+    },
+    
+    // Create the game objects.
+    initObjects : function ()
+    {
+        player = new Player(400, 550, 0.5, 3, playerSpeed, LIVES);
+        game.add.existing(player);
         
-        testObject.damage(3);
+        alien = new Alien(6,6,0.5,0.5,50,65);
+        game.add.existing(alien);
+        alien.visible = false;
+    },
+    
+    hideAlien : function ()
+    {
+        console.log("SPAWNED");
+        alien.SpawnShip();
+    },
+    
+    // Initiliase the input listeners and controls.
+    initInput : function ()
+    {
+        // Create the keyboard controls.
+        cursors = game.input.keyboard.createCursorKeys();
+    },
+    
+    // Initiliase the game's event handlers.
+    initEventHandlers : function ()
+    {
+        //game.time.events.loop(4000, this.removeCharacter);
+        game.time.events.loop(2000, this.hideAlien);
+        //game.time.events.loop(8000, this.addCharacter);
+    },
+    
+    // Initiliase the game's audio.
+    initAudio : function ()
+    {
         
-        testObject.heal(5);
-        
-        notPlayer.notHeal = function (health)
+    },
+    
+    // Initialise the game's user interface.
+    initUI : function ()
+    {
+        // Create a text object to display our fps.
+        fpsText = game.add.text(35, 575, this.game.time.fps, {font: '18px Courier', fill: '#ffffff'});
+    },
+    
+    // Check for constant changes. (Called after start.)
+    update : function ()
+    {
+        // Constantly update our text to display the current game's fps.
+        fpsText.text = "FPS: " + this.game.time.fps;
+        // Poll for input events.
+        this.pollInput();
+    },
+    
+    // Check's for user input events and react based upon it.
+    pollInput : function ()
+    {
+        // Has the user pressed left?
+        if(cursors.left.isDown)
         {
-            console.log(health);
+            // Move the player left.
+            player.MoveLeft();
         }
-        
-        notPlayer.notHeal(4);
-        
-        testObject.notHeal(8);
-        
-    },
-    
-    start : function ()
-    {
-        
-    },
-};
-
-
-
-
-// A barrier object.
-mainObject = function (game, position, anchor) 
-{
-    // Call for a sprite object.
-	//Phaser.Sprite.call(this, game, position * game.width / 5, -20, "Player");
-    Phaser.Sprite.call(this, game, position, 4, "Player");
-    // Enable the physics for the sprite body.
-	game.physics.enable(this, Phaser.Physics.ARCADE);
-    // Set the sprite's inital anchor value.
-    this.anchor.set(anchor, 0.5);
-    
-    this.damage = function (damageDealt)
-    {
-        console.log(damageDealt);
+        // Has the user pressed right?
+        else if(cursors.right.isDown)
+        {
+            // Move the player right.
+            player.MoveRight();
+        }
+        // Has no input event occurred?
+        else 
+        {
+            // Stop the player's movement.
+            player.Stop();
+        }
+        // Has the spacebar been pressed?
+        if(game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR))
+        {
+            // Fire the player's bullet.
+            player.FireBullet();
+            // Flip the canFire flag.
+            player.canFire = false;
+        }
+        // Has the spacebar been released?
+        else 
+        {
+            // Flip the canFire flag.
+            player.canFire = true;
+        }
     }
-};
-
-// Extension of the barrier object to become a Phaser sprite. 
-mainObject.prototype = Object.create(Phaser.Sprite.prototype);
-
-// Create's a constructer from the object's parameters.
-mainObject.prototype.constructor = mainObject;
- 
-// Constant update of the barrier instance.
-mainObject.prototype.update = function () 
-{
-    // Drop the barrier down the screen by it's fall speed.
-	this.body.velocity.y = barrierFallSpeed;
-};
-
-mainObject.prototype.heal = function (healthGiven)
-{
-    console.log(healthGiven);
 };
