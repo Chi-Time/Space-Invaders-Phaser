8d90775b9faf11063de5988c250c044a4c150add
Achromic <music1384@yahoo.co.uk>
2016-03-28 13:52:22 +0100

    Added alien and enemy object prefab skeletons. Have begun work on alien's functionality and testing in tandem with game.js

--- Space Invaders/js/prefabs.js
+++ Space Invaders/js/prefabs.js
@@ -80,6 +80,134 @@ Player.prototype = Object.create(Phaser.Sprite.prototype);
 // The player's default constructor.
 Player.prototype.constructor = Player;
 
+// The enemy object.
+Enemy = function (xPos, yPos, anchorX, anchorY, health, scoreWorth, rowValue, spriteKey)
+{
+    // Call for the sprite object.
+    Phaser.Sprite.call(this, game, xPos, yPos, spriteKey);
+    // Enable arcade physics for the sprite.
+    game.physics.enable(this, Phaser.Physics.ARCADE);
+    // Set the sprite's anchor.
+    this.anchor.set(anchorX, anchorY);
+    
+    // Assign the enemy's variables. \\
+    
+    // The enemy's health value.
+    this.health = health;
+    // The enemy's score worth upon death.
+    this.scoreWorth = scoreWorth;
+    // The row to which this enemy belongs.
+    this.rowValue = rowValue;
+    // Can the enemy fire a bullet?
+    this.canFire = false;
+    
+    // Applies damage to enemy object.
+    this.Damage = function (damageDealt)
+    {
+        this.health -= damageDealt;
+        if(this.health <= 0)
+        {
+            console.log("Enemy has died.");
+        }
+    };
+    
+    // Kill's the enemy object.
+    this.KillEnemy = function ()
+    {
+        this.kill();
+    };
+    
+    // Increase's the player's score.
+    this.IncreaseScore = function ()
+    {
+        
+    };
+    
+    // Set's the enemy below this to be allowed to fire.
+    this.SetPreviousEnemyToFire = function ()
+    {
+        
+    };
+};
+
+Enemy.prototype = Object.create(Phaser.Sprite.prototype);
+Enemy.prototype.constructor = Enemy;
+
+// The alien object.
+Alien = function (xPos, yPos, anchorX, anchorY, scoreWorth, speed)
+{
+    // Call for a sprite object.
+    Phaser.Sprite.call(this, game, xPos, yPos, "Player");
+    // Enable the object's arcade physics.
+    game.physics.enable(this, Phaser.Physics.ARCADE);
+    // Set the object's anchor.
+    this.anchor.set(anchorX, anchorY);
+    
+    //--Assign the alien's variables--\\
+    
+    // The alien's score worth upon death.
+    this.scoreWorth = scoreWorth;
+    // The movement speed of the alien.
+    this.speed = speed;
+    // Is the alien on the left of the screen?
+    this.isLeft = false;
+    
+    // Move's the alien ship.
+    this.Move = function ()
+    {
+        if(this.isLeft)
+        {
+            this.body.velocity.x = speed;
+        }
+        else
+        {
+            this.body.velocity.x = -speed;
+        }
+    };
+    
+    // Spawn's the alien at a random side of the screen.
+    this.SpawnShip = function ()
+    {
+        var randDir = RandomRange(0, 4);
+        
+        if(randDir == 0)
+        {
+            this.isLeft = true;
+            this.reset(0,60);
+            this.Move()
+        }
+        else if( randDir == 1)
+        {
+            this.isLeft = false;
+            this.reset(800,60);
+            this.Move()
+        }
+    };
+    
+    // Increase's the player's score.
+    this.IncreaseScore = function ()
+    {
+        
+    };
+    
+    // Kill's the alien object.
+    this.KillAlien = function ()
+    {
+        
+    };
+    
+    // Kill's the alien object when it has left the screen.
+    this.KillAlienOutOfBounds = function ()
+    {
+        
+    };
+};
+
+// Extend the alien object.
+Alien.prototype = Object.create(Phaser.Sprite.prototype);
+// The alien's default constructor.
+Alien.prototype.constructor = Enemy;
+
 // The bullet object.
 Bullet = function (xPos, yPos, anchor, speed, damageWorth, isEnemyBullet)
 {
@@ -89,6 +217,8 @@ Bullet = function (xPos, yPos, anchor, speed, damageWorth, isEnemyBullet)
 	game.physics.enable(this, Phaser.Physics.ARCADE);
     // Set the sprite's inital anchor value.
     this.anchor.set(anchor, 0.5);
+    
+    //--Assign the bullet's variables--\\
     
     this.speed = speed;
     this.damageWorth = damageWorth;
