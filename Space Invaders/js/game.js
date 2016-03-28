40db5e69704d9335baa6d3111cd2b94348b2f96b
Achromic <music1384@yahoo.co.uk>
2016-03-29 00:57:16 +0100

    Added some more test logic and experimented with collisions triggers as bounds checks. Also added in the render function for eventual physics body tests. Added new event handlers for some small tests.

--- Space Invaders/js/game.js
+++ Space Invaders/js/game.js
@@ -1,3 +1,7 @@
+var leftBoundary;
+var rightBoundary;
+var topBoundary;
+var bottomBoundary;
 // Define the game state.
 GameState = function (game)
 {
@@ -20,6 +24,10 @@ GameState.prototype =
         
         // Enable checking of advancedTiming for debug.
         this.game.time.advancedTiming = true;
+        
+        //dog = game.add.sprite(200,0);
+        
+        alien.SpawnShip();
     },
     
     // Initialise the game's default values.
@@ -47,6 +55,13 @@ GameState.prototype =
     {
         // Start the physics system for use in the whole game.
         game.physics.startSystem(Phaser.Physics.ARCADE);
+        
+        
+    },
+    
+    enableBody : function (bodyToEnable)
+    {
+        game.physics.arcade.enable(bodyToEnable);
     },
     
     // Initiliase the graphics for the game.
@@ -58,18 +73,38 @@ GameState.prototype =
     // Create the game objects.
     initObjects : function ()
     {
+        leftBoundary = game.add.sprite(0, 0);
+        rightBoundary = game.add.sprite(790, 0);
+        topBoundary = game.add.sprite(0, 0);
+        bottomBoundary = game.add.sprite(0, 588);
+        
         player = new Player(400, 550, 0.5, 3, playerSpeed, LIVES);
         game.add.existing(player);
         
-        alien = new Alien(6,6,0.5,0.5,50,65);
+        alien = new Alien(6,6,0.5,0.5,50,100);
         game.add.existing(alien);
         alien.visible = false;
+        
+        this.enableBody(leftBoundary);
+        this.enableBody(rightBoundary);
+        this.enableBody(topBoundary);
+        this.enableBody(bottomBoundary);
+        leftBoundary.body.setSize(10,600, 0, 0);
+        rightBoundary.body.setSize(10,600, 0, 0);
+        topBoundary.body.setSize(800,10, 0, 0);
+        bottomBoundary.body.setSize(800,10, 0, 0);
     },
     
     hideAlien : function ()
     {
         console.log("SPAWNED");
         alien.SpawnShip();
+    },
+    
+    alienDeath : function ()
+    {
+        alien.KillAlien();
+        console.log("DEAD: " + score);
     },
     
     // Initiliase the input listeners and controls.
@@ -79,11 +114,13 @@ GameState.prototype =
         cursors = game.input.keyboard.createCursorKeys();
     },
     
-    // Initiliase the game's event handlers.
+    // Initialise the game's event handlers.
     initEventHandlers : function ()
     {
         //game.time.events.loop(4000, this.removeCharacter);
-        game.time.events.loop(2000, this.hideAlien);
+        //game.time.events.add(1000, this.hideAlien);
+        //game.time.events.add(10000, this.alienDeath);
+        //game.time.events.loop(2000, this.alienDeath);
         //game.time.events.loop(8000, this.addCharacter);
     },
     
@@ -103,10 +140,16 @@ GameState.prototype =
     // Check for constant changes. (Called after start.)
     update : function ()
     {
+        //game.physics.arcade.overlap(alien, leftBoundary, alien.KillAlien, null, this);
+        //game.physics.arcade.overlap(alien, leftBoundary, alien.KillAlien, null, this);
+        //game.physics.arcade.overlap(alien, leftBoundary, alien.KillAlien, null, this);
+        //game.physics.arcade.overlap(alien, leftBoundary, alien.KillAlien, null, this);
         // Constantly update our text to display the current game's fps.
         fpsText.text = "FPS: " + this.game.time.fps;
         // Poll for input events.
         this.pollInput();
+        
+        
     },
     
     // Check's for user input events and react based upon it.
@@ -144,5 +187,19 @@ GameState.prototype =
             // Flip the canFire flag.
             player.canFire = true;
         }
+    },
+    
+    pollAlienPosition : function ()
+    {
+        
+    },
+    
+    render : function ()
+    {
+        // Render the ships body for debug purposes.
+        this.game.debug.body(leftBoundary);
+        this.game.debug.body(rightBoundary);
+        this.game.debug.body(topBoundary);
+        this.game.debug.body(bottomBoundary);
     }
 };
